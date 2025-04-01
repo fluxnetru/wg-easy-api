0986c0323ce3f11d723f9f1bc5a47493e0eef014
Mvory9 <keylipush@gmail.com>
2025-04-01 15:40:04 +0300

    eslint tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,34 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import airbnb from 'eslint-config-airbnb-base';
+import globals from 'globals';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-]);
+export default [
+  {
+    files: ['src/**/*.{js,ts}'],
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      parser: parser,
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    plugins: {
+      '@typescript-eslint': ts
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...airbnb.rules,
+      ...ts.configs.recommended.rules,
+      'no-console': 'warn',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'class-methods-use-this': 'off',
+      'no-throw-literal': 'off'
+    }
+  }
+];
